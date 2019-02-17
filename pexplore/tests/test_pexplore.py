095d1586a3843a922edc6dd45b58c61591af68a2
dave <dave@harpo.marx>
2019-02-17 10:47:40 -0500

    remove test from MANIFEST file.

--- pexplore/tests/test_pexplore.py
+++ pexplore/tests/test_pexplore.py
@@ -3,13 +3,14 @@ import shutil
 import json
 from pexplore import pexplore
 
+
 @pytest.fixture(scope="session")
 def custom_json(tmpdir_factory):
     fn = tmpdir_factory.mktemp("data").join("custom.json")
     shutil.copyfile("pexplore/test_data/custom.json", fn)
     return fn
 
+
 def test_open_psort_json(custom_json):
     jf = pexplore.open_psort_json(custom_json)
     assert type(jf) == dict
-
