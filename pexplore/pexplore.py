59c75f41fbdf1382298286895cc9c810fbd10fa5
dave <dave@harpo.marx>
2019-02-19 21:08:29 -0500

    click stuff

--- pexplore/pexplore.py
+++ pexplore/pexplore.py
@@ -1,29 +1,18 @@
 # -*- coding: utf-8 -*-
-
-import sys
+import click
 import json
 from pprint import pprint
 
-
-def open_psort_json(custom_json):
-    with open(custom_json) as f:
-        pfile = json.load(f)
-        return pfile
-
-
-def get_tags(pfile):
+@click.command()
+@click.argument('input', type=click.File('rb'))
+def cli(input):
+    pfile = json.load(input)
     for session in pfile:
         if pfile[session]["analysis_reports_counter"].get("tagging"):
             tags = pfile[session]["event_labels_counter"]
-    return tags
-
-
-def main():
-    pfile_name = sys.argv[1]
-    pfile = open_psort_json(pfile_name)
-    tags = get_tags(pfile)
     pprint(tags)
 
-
 if __name__ == "__main__":
-    main()
+    cli()
+
+
